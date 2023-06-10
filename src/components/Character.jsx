dc7d20856c66756e25049594785d615cc2c175a6
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-06-10 22:37:08 +0100

    joi and usecallback implemented

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -5,7 +5,6 @@ import Name from "./Name";
 import Quote from "./Quote";
 import Image from "./Image";
 import Delete from "./Delete";
-import { useSelector, useDispatch } from "react-redux";
 
 const Character = ({ item, characterDirection, handleDelete }) => {
   const [liked, setLiked] = useState(false);
